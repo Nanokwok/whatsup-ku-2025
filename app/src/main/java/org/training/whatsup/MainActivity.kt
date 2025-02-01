0d1b7d5da532b0a1522dec68a61b41b4accae23d
Nanokwok <nanokwok3131@gmail.com>
2025-02-02 04:13:03 +0700

    Add Navigation State

--- app/src/main/java/org/training/whatsup/MainActivity.kt
+++ app/src/main/java/org/training/whatsup/MainActivity.kt
@@ -19,8 +19,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -60,11 +65,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun MainScreenWithBottomNavBar() {
     val navController = rememberNavController()
+    var navSelectedItem by remember {
+        mutableStateOf(0)
+    }
     Scaffold( bottomBar = {
         NavigationBar {
             WhatsUpNavItemInfo().getAllNavItems().forEachIndexed { index, itemInfo ->
-                NavigationBarItem( selected = false,
-                    onClick = { /*TODO*/ },
+                NavigationBarItem( selected = (index == navSelectedItem),
+                    onClick = {
+                        navSelectedItem = index
+                        navController.navigate(itemInfo.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
                     icon = { Icon(imageVector = itemInfo.icon, contentDescription = itemInfo.label) },
                     label = { Text(text = itemInfo.label) } )
             }
