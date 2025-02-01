853dc06d1ee3e4b9f1521a373f365db19231068e
Nanokwok <nanokwok3131@gmail.com>
2025-02-02 02:10:02 +0700

    Done SignIn

--- app/src/main/java/org/training/whatsup/LogInVeiwActivity.kt
+++ app/src/main/java/org/training/whatsup/LogInVeiwActivity.kt
@@ -3,6 +3,8 @@ package org.training.whatsup
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
@@ -21,6 +23,22 @@ class LogInVeiwActivity : AppCompatActivity() {
 //        }
         val signInBtn:Button = findViewById(R.id.signInBtn)
         val signInPanel:CardView = findViewById(R.id.signInPanel)
+        val usernameTxt:EditText = findViewById(R.id.usernameTxt)
+        val pwdTxt:EditText = findViewById(R.id.pwdTxt)
+        val enterBtn:Button = findViewById(R.id.enterBtn)
         signInBtn.setOnClickListener { signInPanel.visibility = View.VISIBLE }
+        var username:String; var password:String
+        enterBtn.setOnClickListener {
+            username = usernameTxt.text.toString()
+            password = pwdTxt.text.toString()
+            if (username.equals("android")&&password.equals("google"))
+            {
+                Toast.makeText(this, "Welcome to WhatUp!!", Toast.LENGTH_LONG).show()
+                usernameTxt.setText("")
+                pwdTxt.setText("")
+                signInPanel.visibility = View.GONE
+            }
+            else Toast.makeText(this, "Please Try Again", Toast.LENGTH_LONG).show()
+        }
     }
 }
