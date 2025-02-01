679f7d97ed35c68f5ea7638b15beca075082ab96
Nanokwok <nanokwok3131@gmail.com>
2025-02-02 03:50:23 +0700

    Create MyEventsScreen

---
+++ app/src/main/java/org/training/whatsup/MyEventsScreen.kt
@@ -0,0 +1,34 @@
+package org.training.whatsup
+
+import android.view.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.training.whatsup.ui.theme.WhatsUpTheme
+import androidx.compose.material3.Text
+
+@Composable
+fun MyEventsScreen() {
+    WhatsUpTheme {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(4.dp),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column (modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = "Events to Attend")
+            }
+        }
+    }
+}
